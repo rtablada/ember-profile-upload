0be2af44e7d81430dd5dc2d0ef5ef486f030a400
Ryan Tablada <ryan.tablada@gmail.com>
2016-05-10 10:01:39 -0500

    wip: show existing image

--- tests/integration/components/profile-upload-test.js
+++ tests/integration/components/profile-upload-test.js
@@ -5,20 +5,11 @@ moduleForComponent('profile-upload', 'Integration | Component | profile upload',
   integration: true
 });
 
-test('it renders', function(assert) {
+test('it renders existing image', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{profile-upload}}`);
+  this.render(hbs`{{profile-upload src="img.png"}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#profile-upload}}
-      template block text
-    {{/profile-upload}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$(`img`).attr(`src`), 'img.png');
 });
